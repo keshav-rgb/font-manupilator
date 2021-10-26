0a1644c2f657910636dba744c7392a2225f8dfcf
keshav-rgb <78646085+keshav-rgb@users.noreply.github.com>
2021-10-26 22:10:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,10 +9,19 @@ function setup(){
     poseNet.on('pose',gotposes);
 }
 function modeloaded(){
-console.log('poseNet is initialized')
+console.log('poseNet is initialized');
 }
 function gotposes(results){
-if(results.length > 0){
-console.log(results);
+    if(results.length > 0){
+        console.log(results);
+        leftwristx = results[0].pose.leftwristx.x;
+        rightwristx = results[0].pose.rightwristx.x;
+        difference = floor(leftwristx-rightwristx)
+        }
 }
+function draw(){
+background('#6C91C2');
+textSize(10);
+fill('#FFE787');
+text('peter',50,400);
 }
